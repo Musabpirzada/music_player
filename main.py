065e46244fe4cda9090658750d056a0e6d8b2282
Musab <musabpirzada1@gmail.com>
2023-04-10 15:27:16 +0500

    Make of tkinter window

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+from tkinter import *
+from tkinter import filedialog
+from pygame import mixer
+import os
+
+root = Tk()
+root.title("FoxJox Player")
+root.geometry("485x700+100+10")
+root.configure(background="#333333")
+root.mainloop()
