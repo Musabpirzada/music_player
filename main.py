182cb0a11f45047d67e32b8e5dccdd106c6cca90
Musab <musabpirzada1@gmail.com>
2023-04-10 15:31:26 +0500

    added frame for buttons

--- main.py
+++ main.py
@@ -8,4 +8,8 @@ root = Tk()
 root.title("FoxJox Player")
 root.geometry("485x700+100+10")
 root.configure(background="#333333")
+root.resizable(False, False)
+
+bottom_frame = Frame(root, bg="#FFFFFF", width=485, height=180)
+bottom_frame.place(x=0, y=400)
 root.mainloop()
