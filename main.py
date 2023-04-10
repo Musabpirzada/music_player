03c18efa209e788572eb7ca5e285a81d3fefaa21
Musab <musabpirzada1@gmail.com>
2023-04-10 15:47:59 +0500

    added more frames and button and scrollbar

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ root.geometry("485x700+100+10")
 root.configure(background="#333333")
 root.resizable(False, False)
 
+
 bottom_frame = Frame(root, bg="#FFFFFF", width=485, height=180)
 bottom_frame.place(x=0, y=400)
+
+logo = PhotoImage(file="logo.png")
+root.iconphoto(False, logo)
+
+Menu = PhotoImage(file = "")
+Label(root, image= Menu).place(x=0, y=580, width=485, height=100)
+frame_music = Frame(root, bd = 2, relief=RIDGE)
+frame_music.place(x=0,y=585, width=485, height=100)
+Button(root, text="Browse Music", width=59, height=1, font=("calibri", 12, "bold"), fg="black", bg="#FFFFFF", command = AddMusic).place(x=0,y=550))
+Scrollbar(frame_music)
+
+
+
+
 root.mainloop()
